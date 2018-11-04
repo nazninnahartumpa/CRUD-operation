6d45f39365c6a1f8d736dfa065a3cad04d1bdc9b
Naznin Nahar Tumpa <naznintumpa5@gmail.com>
2018-11-05 05:36:02 +0600

    Class based view and crud operation

--- CBV_app/views.py
+++ CBV_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.views.generic import View,TemplateView,ListView,DateDetailView
+from django.views.generic import (View,TemplateView,
+										ListView,DetailView,
+										CreateView,UpdateView,
+										DeleteView)
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from . import models
 
@@ -21,7 +25,22 @@ class SchoolListView(ListView):
     context_object_name = "schools"
     model = models.School
 
-class SchoolDetailView(DateDetailView):
+
+class SchoolDetailView(DetailView):
     context_object_name = "school_detail"
     model = models.School
     template_name = 'CBV_app/school_details.html'
+
+
+class SchoolCreateView(CreateView):
+	fields = ('name','principle','locations')
+	model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+	fields = ('name','principle')
+	model = models.School
+
+class SchoolDeleteView(DeleteView):
+	model = models.School
+	success_url = reverse_lazy("CBV_app:list")
